f6520114743fff31eaf433bc0cb5c30fec8087b3
Pasha <pasha@gmail.com>
2015-03-01 00:07:15 +0200

    Change .gitignore and voice-memos components

--- test/unit/session/addSession.js
+++ test/unit/session/addSession.js
@@ -7,9 +7,9 @@ var Session = require('../../../lib/vm-api/services/Session');
 describe('SessionService#addSession()', function() {
   it('should add new session', function(done) {
     Session.addSession('TestUser', '1234', 'qweq123123.ewfweuasvEWBFYUSDYUA', function (error, result) {
-  	  should.not.exist(error);
-			result[0].username.should.equal('TestUser');
-			done();
+  	  	should.not.exist(error);
+		result[0].username.should.equal('TestUser');
+		done();
     });
   });
 
