ccc73c038c5bddb3637e60dbce265469eb9c47ca
Zaratustra <zakhar.idzinsky@gmail.com>
2015-03-11 16:12:03 +0200

    Change target api

--- test/unit/target/setTarget.js
+++ test/unit/target/setTarget.js
@@ -18,17 +18,7 @@ describe('#setTarget()', function() {
 	it('should update target', function(done){
 		var Target = require(targetService);
 
-		Target.setTargetLongitude(40.12, 22, function(error, result) {
-			should.not.exist(error);
-			result.should.equal(1);
-			done();
-		});
-	});
-
-	it('should update target', function(done){
-		var Target = require(targetService);
-
-		Target.setTargetLatitude(70.11, 22, function(error, result) {
+		Target.setTargetCoordinates(40.12,  50.22, 22, function(error, result) {
 			should.not.exist(error);
 			result.should.equal(1);
 			done();
