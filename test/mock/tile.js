fa1fc60c9c980d40ab690942d7159dac3e70efce
Zaratustra <zakhar.idzinsky@gmail.com>
2015-03-19 21:28:26 +0200

    add tile service

---
+++ test/mock/tile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var Tile = {
+	add: function(username, tileName, tileStatus, callback) {
+		var result = 1;
+		return callback(null, result);
+	},
+
+	get: function(username, tileName, callback) {
+		var result = 1;
+		return callback(null, result);
+	},
+
+	delete: function(username, callback) {
+		var result = 1;
+		return callback(null, result);
+	}
+};
